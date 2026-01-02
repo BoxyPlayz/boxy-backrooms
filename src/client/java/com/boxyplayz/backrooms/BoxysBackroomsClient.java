57013149dff9729076b5a5459b31ae06c18af994
Boxy <boxy@myyahoo.com>
2026-01-02 03:16:21 -0600

    the whole ahh mod

---
+++ src/client/java/com/boxyplayz/backrooms/BoxysBackroomsClient.java
@@ -0,0 +1,10 @@
+package com.boxyplayz.backrooms;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BoxysBackroomsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
