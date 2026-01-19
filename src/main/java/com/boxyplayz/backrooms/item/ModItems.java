76dbc80459002b9057dd03cb9b1575b9ec0e1bfc
Boxy <boxy@myyahoo.com>
2026-01-19 13:12:39 -0600

    IT WORKSS!!!!!!!!!!! :D

--- src/main/java/com/boxyplayz/backrooms/item/ModItems.java
+++ src/main/java/com/boxyplayz/backrooms/item/ModItems.java
@@ -9,8 +9,13 @@ import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.component.Consumables;
+import net.minecraft.world.item.consume_effects.ApplyStatusEffectsConsumeEffect;
 
 public class ModItems {
 	private static Item registerItem(@NonNull String name, Item item) {
@@ -21,12 +26,19 @@ public class ModItems {
 	public static Item ALMOND_WATER;
 
 	public static void registerModItems() {
+
 		BoxysBackrooms.LOGGER.info("Registering items for " + BoxysBackrooms.MOD_ID);
 
 		ResourceKey<Item> key = ResourceKey.create(
 				BuiltInRegistries.ITEM.key(),
 				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "almond_water"));
-		ALMOND_WATER = registerItem("almond_water", new Item(new Item.Properties().setId(key)));
+		ALMOND_WATER = registerItem("almond_water", new Item(new Item.Properties()
+				.food(new FoodProperties.Builder().nutrition(5).saturationModifier(8).build(),
+						Consumables.defaultFood()
+								.onConsume(new ApplyStatusEffectsConsumeEffect(
+										new MobEffectInstance(MobEffects.REGENERATION, 5 * 20, 5)))
+								.build())
+				.setId(key)));
 
 		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.INGREDIENTS).register(itemGroup -> {
 			itemGroup.accept(ALMOND_WATER);
