5b21b54088d8cbdb7f677947a679870fa5901e13
Boxy <boxy@myyahoo.com>
2026-02-19 00:09:27 -0600

    Errorslate

--- src/main/java/com/boxyplayz/backrooms/BoxysBackrooms.java
+++ src/main/java/com/boxyplayz/backrooms/BoxysBackrooms.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.boxyplayz.backrooms.block.ModBlocks;
 import com.boxyplayz.backrooms.creativetabs.ModCreativeTabs;
 import com.boxyplayz.backrooms.entity.ModEntities;
 import com.boxyplayz.backrooms.item.ModItems;
@@ -18,5 +19,6 @@ public class BoxysBackrooms implements ModInitializer {
 		ModItems.registerModItems();
 		ModCreativeTabs.RegisterModCreativeTabs();
 		ModEntities.RegisterModEntities();
+		ModBlocks.RegisterModBlocks();
 	}
 }
