343210f8980acf0e9e686e4ac9a80340933e8825
Boxy <boxy@myyahoo.com>
2026-03-06 21:27:29 -0600

    Firesalt recipe

--- src/client/java/com/boxyplayz/backrooms/datagen/BoxyBackroomsRecipeProvider.java
+++ src/client/java/com/boxyplayz/backrooms/datagen/BoxyBackroomsRecipeProvider.java
@@ -3,6 +3,7 @@ package com.boxyplayz.backrooms.datagen;
 import java.util.concurrent.CompletableFuture;
 
 import com.boxyplayz.backrooms.block.ModBlocks;
+import com.boxyplayz.backrooms.item.ModItems;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
@@ -12,8 +13,10 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.SimpleCookingRecipeBuilder;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 
 public class BoxyBackroomsRecipeProvider extends FabricRecipeProvider {
 
@@ -33,11 +36,16 @@ public class BoxyBackroomsRecipeProvider extends FabricRecipeProvider {
 			public void buildRecipes() {
 				HolderLookup.RegistryLookup<Item> itemLookup = registries.lookupOrThrow(Registries.ITEM);
 				shapeless(RecipeCategory.MISC, ModBlocks.LEVEL0_WALLPAPER)
-				.requires(Items.YELLOW_CONCRETE)
-				.requires(Items.AMETHYST_SHARD, 4)
-				.requires(Items.ENDER_PEARL)
-				.unlockedBy(getHasName(Items.YELLOW_CONCRETE), has(Items.YELLOW_CONCRETE))
-				.save(output);
+						.requires(Items.YELLOW_CONCRETE)
+						.requires(Items.AMETHYST_SHARD, 4)
+						.requires(Items.ENDER_PEARL)
+						.unlockedBy(getHasName(Items.YELLOW_CONCRETE), has(Items.YELLOW_CONCRETE))
+						.save(output);
+
+				SimpleCookingRecipeBuilder.blasting(Ingredient.of(Items.AMETHYST_SHARD), RecipeCategory.COMBAT,
+						ModItems.FIRESALT_SHARD, 0.1f, 100)
+						.unlockedBy(getHasName(Items.AMETHYST_SHARD), has(Items.AMETHYST_SHARD))
+						.save(output);
 			}
 		};
 	}
