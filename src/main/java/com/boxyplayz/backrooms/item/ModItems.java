ca7cf159e995892b1ce96c7e5ae3f6dfe2b0992c
Boxy <boxy@myyahoo.com>
2026-04-01 17:36:32 -0500

    e

--- src/main/java/com/boxyplayz/backrooms/item/ModItems.java
+++ src/main/java/com/boxyplayz/backrooms/item/ModItems.java
@@ -1,5 +1,7 @@
 package com.boxyplayz.backrooms.item;
 
+import java.util.function.Function;
+
 import org.jspecify.annotations.NonNull;
 
 import com.boxyplayz.backrooms.BoxysBackrooms;
@@ -17,93 +19,63 @@ import net.minecraft.world.item.component.Consumables;
 import net.minecraft.world.item.consume_effects.ApplyStatusEffectsConsumeEffect;
 
 public class ModItems {
-	private static Item registerItem(@NonNull String name, Item item) {
+	private static Item registerItem(@NonNull String name, Function<Item.Properties, Item> itemFactory,
+			Item.Properties settings) {
 		Identifier id = Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, name);
+		ResourceKey<Item> key = ResourceKey.create(
+				BuiltInRegistries.ITEM.key(), id);
+		Item item = itemFactory.apply(settings.setId(key));
 		return Registry.register(BuiltInRegistries.ITEM, id, item);
 	}
 
-	public static Item GRAY_ALMOND_WATER, GREEN_ALMOND_WATER, RED_ALMOND_WATER, ROYAL_RATION, FIRESALT_SHARD,
-			SHADOW_DUST, SMILER_REPELLANT, FIRESTEEL_ALLOY, FIRESTEEL_SWORD;
+	public static final Item GRAY_ALMOND_WATER = registerItem("gray_almond_water", Item::new, new Item.Properties()
+			.food(new FoodProperties.Builder().nutrition(5).saturationModifier(1f).alwaysEdible().build()));
+
+	public static final Item ROYAL_RATION = registerItem("royal_ration", Item::new, new Item.Properties()
+			.food(new FoodProperties.Builder().nutrition(20).saturationModifier(8f).build()));
+
+	public static final Item GREEN_ALMOND_WATER = registerItem("green_almond_water", Item::new, new Item.Properties()
+			.food(new FoodProperties.Builder().nutrition(4).saturationModifier(1f).alwaysEdible().build(),
+					Consumables.defaultFood()
+							.onConsume(new ApplyStatusEffectsConsumeEffect(
+									new MobEffectInstance(MobEffects.SPEED, 10 * 20, 2)))
+							.onConsume(new ApplyStatusEffectsConsumeEffect(
+									new MobEffectInstance(MobEffects.NAUSEA, 11 * 20, 1)))
+							.build()));
+
+	public static final Item RED_ALMOND_WATER = registerItem("red_almond_water", Item::new, new Item.Properties()
+			.food(new FoodProperties.Builder().nutrition(4).saturationModifier(1f).alwaysEdible().build(),
+					Consumables.defaultFood()
+							.onConsume(new ApplyStatusEffectsConsumeEffect(
+									new MobEffectInstance(MobEffects.SLOWNESS, 30 * 20, 1)))
+							.onConsume(new ApplyStatusEffectsConsumeEffect(
+									new MobEffectInstance(MobEffects.REGENERATION, 25 * 20, 2)))
+							.onConsume(new ApplyStatusEffectsConsumeEffect(
+									new MobEffectInstance(MobEffects.MINING_FATIGUE, 30 * 20, 1)))
+							.onConsume(new ApplyStatusEffectsConsumeEffect(
+									new MobEffectInstance(MobEffects.WEAKNESS, 30 * 20, 3)))
+							.build()));
+
+	public static final Item FIRESALT_SHARD = registerItem("firesalt_shard",
+			FireSaltItem::new, new Item.Properties());
+
+	public static final Item SHADOW_DUST = registerItem("shadow_dust",
+			Item::new, new Item.Properties());
+
+	public static final Item SMILER_REPELLANT = registerItem("smiler_repellant",
+			Item::new, new Item.Properties());
+
+	public static final Item FIRESTEEL_ALLOY = registerItem("firesteel_alloy",
+			Item::new, new Item.Properties());
+
+	public static final Item FIRESTEEL_SWORD = registerItem("firesteel_sword",
+			FireSaltItem::new,
+			new Item.Properties().sword(ModToolMaterials.FIRESTEEL_MATERIAL, 3,
+					-2));
 
 	public static void registerModItems() {
 
 		BoxysBackrooms.LOGGER.debug("Registering items for " + BoxysBackrooms.MOD_ID);
-
-		ResourceKey<Item> grayAlmondWaterKey = ResourceKey.create(
-				BuiltInRegistries.ITEM.key(),
-				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "gray_almond_water"));
-		GRAY_ALMOND_WATER = registerItem("gray_almond_water", new Item(new Item.Properties()
-				.food(new FoodProperties.Builder().nutrition(5).saturationModifier(1f).alwaysEdible().build())
-				.setId(grayAlmondWaterKey)));
-
-		ResourceKey<Item> royalRationKey = ResourceKey.create(
-				BuiltInRegistries.ITEM.key(),
-				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "royal_ration"));
-		ROYAL_RATION = registerItem("royal_ration", new Item(new Item.Properties()
-				.food(new FoodProperties.Builder().nutrition(20).saturationModifier(8f).build())
-				.setId(royalRationKey)));
-
-		ResourceKey<Item> greenAlmondWaterKey = ResourceKey.create(
-				BuiltInRegistries.ITEM.key(),
-				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "green_almond_water"));
-		GREEN_ALMOND_WATER = registerItem("green_almond_water", new Item(new Item.Properties()
-				.food(new FoodProperties.Builder().nutrition(4).saturationModifier(1f).alwaysEdible().build(),
-						Consumables.defaultFood()
-								.onConsume(new ApplyStatusEffectsConsumeEffect(
-										new MobEffectInstance(MobEffects.SPEED, 10 * 20, 2)))
-								.onConsume(new ApplyStatusEffectsConsumeEffect(
-										new MobEffectInstance(MobEffects.NAUSEA, 11 * 20, 1)))
-								.build())
-				.setId(greenAlmondWaterKey)));
-
-		ResourceKey<Item> redAlmondWaterKey = ResourceKey.create(
-				BuiltInRegistries.ITEM.key(),
-				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "red_almond_water"));
-		RED_ALMOND_WATER = registerItem("red_almond_water", new Item(new Item.Properties()
-				.food(new FoodProperties.Builder().nutrition(4).saturationModifier(1f).alwaysEdible().build(),
-						Consumables.defaultFood()
-								.onConsume(new ApplyStatusEffectsConsumeEffect(
-										new MobEffectInstance(MobEffects.SLOWNESS, 30 * 20, 1)))
-								.onConsume(new ApplyStatusEffectsConsumeEffect(
-										new MobEffectInstance(MobEffects.REGENERATION, 25 * 20, 2)))
-								.onConsume(new ApplyStatusEffectsConsumeEffect(
-										new MobEffectInstance(MobEffects.MINING_FATIGUE, 30 * 20, 1)))
-								.onConsume(new ApplyStatusEffectsConsumeEffect(
-										new MobEffectInstance(MobEffects.WEAKNESS, 30 * 20, 3)))
-								.build())
-				.setId(redAlmondWaterKey)));
-
-		ResourceKey<Item> fireSaltShardKey = ResourceKey.create(
-				BuiltInRegistries.ITEM.key(),
-				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "firesalt_shard"));
-		FIRESALT_SHARD = registerItem("firesalt_shard",
-				new FireSaltItem(new Item.Properties().setId(fireSaltShardKey)));
-
-		ResourceKey<Item> shadowDustKey = ResourceKey.create(
-				BuiltInRegistries.ITEM.key(),
-				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "shadow_dust"));
-		SHADOW_DUST = registerItem("shadow_dust",
-				new Item(new Item.Properties().setId(shadowDustKey)));
-
-		ResourceKey<Item> smilerRepellantKey = ResourceKey.create(
-				BuiltInRegistries.ITEM.key(),
-				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "smiler_repellant"));
-		SMILER_REPELLANT = registerItem("smiler_repellant",
-				new Item(new Item.Properties().setId(smilerRepellantKey)));
-
-		ResourceKey<Item> fireSteelAlloyKey = ResourceKey.create(
-				BuiltInRegistries.ITEM.key(),
-				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "firesteel_alloy"));
-		FIRESTEEL_ALLOY = registerItem("firesteel_alloy",
-				new Item(new Item.Properties().setId(fireSteelAlloyKey)));
-
-		ResourceKey<Item> fireSteelSwordKey = ResourceKey.create(
-				BuiltInRegistries.ITEM.key(),
-				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "firesteel_sword"));
-		FIRESTEEL_SWORD = registerItem("firesteel_sword",
-				new FireSaltItem(
-						new Item.Properties().setId(fireSteelSwordKey).sword(ModToolMaterials.FIRESTEEL_MATERIAL, 3,
-								-2)));
 
 	}
 }
