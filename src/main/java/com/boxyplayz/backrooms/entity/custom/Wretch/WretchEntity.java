90cb1931178f663191929ea5cccbf91f4557c327
Boxy <boxy@myyahoo.com>
2026-04-11 14:16:57 -0500

    THEY'RE CONTAGIOUS!

--- src/main/java/com/boxyplayz/backrooms/entity/custom/Wretch/WretchEntity.java
+++ src/main/java/com/boxyplayz/backrooms/entity/custom/Wretch/WretchEntity.java
@@ -1,9 +1,15 @@
 package com.boxyplayz.backrooms.entity.custom.Wretch;
 
+import com.boxyplayz.backrooms.effect.ModEffects;
+
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntitySpawnReason;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
@@ -42,5 +48,13 @@ public class WretchEntity extends PathfinderMob {
 				.add(Attributes.MOVEMENT_SPEED, 0.39D)
 				.add(Attributes.ATTACK_DAMAGE, 16.0D)
 				.add(Attributes.FOLLOW_RANGE, 20.0D);
+	}
+
+	@Override
+	public boolean doHurtTarget(ServerLevel level, Entity target) {
+		if (target instanceof LivingEntity entity) {
+			entity.addEffect(new MobEffectInstance(ModEffects.WRETCHED_CYCLE, 20 * 60));
+		}
+		return super.doHurtTarget(level, target);
 	}
 }
