cb90c30c222f52d448d488377c2544107f5c1d4d
Boxy <boxy@myyahoo.com>
2026-04-11 15:56:13 -0500

    Decrease Version for emergency fixes

--- src/client/java/com/boxyplayz/backrooms/entity/skinstealer/SkinStealerEntityRenderer.java
+++ src/client/java/com/boxyplayz/backrooms/entity/skinstealer/SkinStealerEntityRenderer.java
@@ -3,14 +3,10 @@ package com.boxyplayz.backrooms.entity.skinstealer;
 import com.boxyplayz.backrooms.BoxysBackrooms;
 import com.boxyplayz.backrooms.BoxysBackroomsClient;
 import com.boxyplayz.backrooms.entity.custom.SkinStealer.SkinStealerEntity;
-import com.mojang.authlib.GameProfile;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
-import net.minecraft.client.resources.SkinManager;
 import net.minecraft.resources.Identifier;
-import net.minecraft.world.entity.player.PlayerSkin;
 
 public class SkinStealerEntityRenderer
 		extends MobRenderer<SkinStealerEntity, SkinStealerRenderState, SkinStealerModel<SkinStealerEntity>> {
@@ -24,29 +20,7 @@ public class SkinStealerEntityRenderer
 
 	@Override
 	public Identifier getTextureLocation(SkinStealerRenderState renderState) {
-		if (renderState.cachedSkin != null) {
-			return renderState.cachedSkin;
-		}
-		if (renderState.maskedPlayerUsername != null &&
-				!renderState.maskedPlayerUsername.isEmpty() && renderState.maskedPlayerUUID != null) {
-			Minecraft client = Minecraft.getInstance();
-			SkinManager manager = client.getSkinManager();
-			GameProfile profile = new GameProfile(renderState.maskedPlayerUUID, renderState.maskedPlayerUsername);
-			PlayerSkin skin = manager.createLookup(profile, false).get();
-			Identifier texture = skin.body().texturePath();
-			renderState.cachedSkin = texture;
-			return texture;
-		}
 		return Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "textures/entity/skinstealer.png");
-	}
-
-	@Override
-	public void extractRenderState(SkinStealerEntity entity, SkinStealerRenderState state, float partialTicks) {
-		if (entity.getPlayerKilled() != null && !entity.getPlayerKilled().isEmpty()) {
-			state.maskedPlayerUsername = entity.getPlayerKilled();
-			state.maskedPlayerUUID = entity.getPlayerKilledUUID();
-		}
-		super.extractRenderState(entity, state, partialTicks);
 	}
 
 	@Override
--- src/client/java/com/boxyplayz/backrooms/entity/skinstealer/SkinStealerRenderState.java
+++ src/client/java/com/boxyplayz/backrooms/entity/skinstealer/SkinStealerRenderState.java
@@ -1,24 +1,9 @@
 package com.boxyplayz.backrooms.entity.skinstealer;
 
-import java.util.UUID;
-
-import org.jetbrains.annotations.Nullable;
-
-import com.boxyplayz.backrooms.entity.custom.SkinStealer.SkinStealerVarient;
-
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.renderer.entity.state.HumanoidRenderState;
-import net.minecraft.resources.Identifier;
 
 @Environment(EnvType.CLIENT)
 public class SkinStealerRenderState extends HumanoidRenderState {
-	@Nullable
-	public String maskedPlayerUsername;
-	@Nullable
-	public UUID maskedPlayerUUID;
-	@Nullable
-	public SkinStealerVarient varient;
-	@Nullable
-	public Identifier cachedSkin;
 }
