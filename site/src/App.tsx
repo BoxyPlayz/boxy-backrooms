b33d759a4fdc7bfd9965972a779e22f09e3abeb3
Boxy <boxy@myyahoo.com>
2026-04-19 12:25:52 -0500

    New Home

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,6 +1,3 @@
-
-import reactLogo from './assets/react.svg';
-import viteLogo from './assets/vite.svg';
 import './styles/App.css';
 import MiniSearch, { type SearchResult } from 'minisearch';
 import { useEffect, useRef, useState } from 'react';
@@ -52,42 +49,8 @@ function App() {
 				</div>
 			</section>
 
+				<p>The backrooms is a dangerous place outside of reality. However, if you're careful, it can also be a gold mine.</p>
 				<section id='next-steps'>
-					<div id='docs'>
-						<svg
-							className='icon'
-							role='presentation'
-							aria-hidden='true'>
-							<use href='/boxy-backrooms/icons.svg#documentation-icon'></use>
-						</svg>
-						<h2>Resources</h2>
-						<ul>
-							<li>
-								<a
-									href='https://vite.dev/'
-									target='_blank'>
-									<img
-										className='logo'
-										src={viteLogo}
-										alt=''
-									/>
-									Explore Vite
-								</a>
-							</li>
-							<li>
-								<a
-									href='https://react.dev/'
-									target='_blank'>
-									<img
-										className='button-icon'
-										src={reactLogo}
-										alt=''
-									/>
-									Learn more
-								</a>
-							</li>
-						</ul>
-					</div>
 					<div id='social'>
 						<h2>Connect with us</h2>
 						<ul>
