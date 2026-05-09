eca6a0888eed0149e89213d6fe622e610c672615
Boxy <boxy@myyahoo.com>
2026-05-09 02:00:40 -0500

    Newlines. thats actually just it.

--- src/main/java/com/boxyplayz/backrooms/world/generators/ModChunkGenerators.java
+++ src/main/java/com/boxyplayz/backrooms/world/generators/ModChunkGenerators.java
@@ -9,19 +9,26 @@ import net.minecraft.resources.Identifier;
 public class ModChunkGenerators {
 	public static void registerModChunkGenerators() {
 		Registry.register(BuiltInRegistries.CHUNK_GENERATOR,
-				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "level0_maze"), Level0ChunkGen.CODEC);
+				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "level0_maze"),
+				Level0ChunkGen.CODEC);
 		Registry.register(BuiltInRegistries.CHUNK_GENERATOR,
-				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "level7_ocean"), Level7ChunkGen.CODEC);
+				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "level7_ocean"),
+				Level7ChunkGen.CODEC);
 		Registry.register(BuiltInRegistries.CHUNK_GENERATOR,
-				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "level94"), Level94ChunkGen.CODEC);
+				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "level94"),
+				Level94ChunkGen.CODEC);
 		Registry.register(BuiltInRegistries.CHUNK_GENERATOR,
-				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "the_broken"), TheBrokenChunkGen.CODEC);
+				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "the_broken"),
+				TheBrokenChunkGen.CODEC);
 		Registry.register(BuiltInRegistries.CHUNK_GENERATOR,
-				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "level1"), Level1ChunkGen.CODEC);
+				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "level1"),
+				Level1ChunkGen.CODEC);
 		Registry.register(BuiltInRegistries.CHUNK_GENERATOR,
-				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "pitfalls"), PitfallsChunkGen.CODEC);
+				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "pitfalls"),
+				PitfallsChunkGen.CODEC);
 		Registry.register(BuiltInRegistries.CHUNK_GENERATOR,
-				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "level0_2"), Level0_2ChunkGen.CODEC);
+				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "level0_2"),
+				Level0_2ChunkGen.CODEC);
 		Registry.register(BuiltInRegistries.CHUNK_GENERATOR,
 				Identifier.fromNamespaceAndPath(BoxysBackrooms.MOD_ID, "negative_level0_2"),
 				Level_Negative_0_2ChunkGen.CODEC);
